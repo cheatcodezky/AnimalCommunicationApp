7fad8a1aa1cd46253839e33f7fc80f275329bec6
unknown <951127336@qq.com>
2018-06-07 08:54:25 +0800

    更新了个人信息修改界面的UI informationPage

---
+++ app/src/main/java/com/treeanimals/max/animal/InformationActivtiy.java
@@ -0,0 +1,16 @@
+package com.treeanimals.max.animal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 95112 on 2018/6/7.
+ */
+
+public class InformationActivtiy extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.information_page);
+    }
+}
--- app/src/main/java/com/treeanimals/max/animal/UserFragment.java
+++ app/src/main/java/com/treeanimals/max/animal/UserFragment.java
@@ -1,22 +1,45 @@
 package com.treeanimals.max.animal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by 95112 on 2018/6/6.
  */
 
 public class UserFragment extends Fragment {
-
+    public static final int INFORMATIONCODE = 1;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_page,container,false);
+        LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.userInformation);
+        linearLayout.setOnClickListener(new ClickListener());
         return view;
+    }
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+
+    }
+    class ClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.userInformation:
+                    Intent intent = new Intent(getContext(),InformationActivtiy.class);
+                    startActivityForResult(intent,INFORMATIONCODE);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
