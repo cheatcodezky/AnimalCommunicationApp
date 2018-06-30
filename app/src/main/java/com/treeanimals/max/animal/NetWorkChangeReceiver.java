4c377874227a1717f3268340b8bf0a3ad93d1e5e
unknown <951127336@qq.com>
2018-06-30 08:03:02 +0800

    广播管理类

---
+++ app/src/main/java/com/treeanimals/max/animal/NetWorkChangeReceiver.java
@@ -0,0 +1,16 @@
+package com.treeanimals.max.animal;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by 95112 on 2018/6/30.
+ */
+
+public class NetWorkChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
